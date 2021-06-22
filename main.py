7efee9dc0a9cbce268579841050b64fbb01a8bcf
Krystian <krystianbezpalko@gmail.com>
2021-06-22 21:01:43 +0200

    Moduł 10.2

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies=['elo', '320', 'XD']
-    return render_template("homepage.html", movies = movies)
+    movies = [1, 2, 3, 4]
+    return render_template("homepage.html", movies=movies)
 
 if __name__ == "__main__":
     app.run(debug=True)
